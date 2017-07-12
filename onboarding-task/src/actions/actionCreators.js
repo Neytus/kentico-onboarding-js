6955a05d3c1e546311f86b7c9fe6e393af6d80ab
Neytus <jbielik44@gmail.com>
2017-07-12 16:32:52 +0200

    KA-152 Add basic reducer structure

--- onboarding-task/src/actions/actionCreators.js
+++ onboarding-task/src/actions/actionCreators.js
@@ -1,32 +1,37 @@
-import { ADD_NODE, TOGGLE_NODE, SAVE_NODE, DELETE_NODE } from './actionTypes';
+import {
+  ADD_NODE,
+  TOGGLE_NODE,
+  SAVE_NODE,
+  DELETE_NODE,
+} from './actionTypes';
 import { generateId } from '../utils/generateId';
 
-export function addNode(text) {
+export const addNode = text => {
   return {
     type: ADD_NODE,
     id: generateId(),
     text,
   };
-}
+};
 
-export function toggleNode(id) {
+export const toggleNode = id => {
   return {
     type: TOGGLE_NODE,
     id,
   };
-}
+};
 
-export function deleteNode(id) {
+export const deleteNode = id => {
   return {
     type: DELETE_NODE,
     id,
   };
-}
+};
 
-export function saveNode(id, text) {
+export const saveNode = (id, text) => {
   return {
     type: SAVE_NODE,
     id,
     text,
   };
-}
+};
--- onboarding-task/src/actions/reducers.js
+++ onboarding-task/src/actions/reducers.js
@@ -1,6 +1,12 @@
-import { ADD_NODE, TOGGLE_NODE, SAVE_NODE, DELETE_NODE } from './actionTypes';
-import { addNode, toggleNode, saveNode, deleteNode } from './actionCreators';
+import {
+  ADD_NODE,
+  TOGGLE_NODE,
+  SAVE_NODE,
+  DELETE_NODE,
+} from './actionTypes';
 import { OrderedMap } from 'immutable';
+import { NodeContent } from '../models/NodeContent';
+import { NodeInfo } from '../models/NodeInfo';
 
 const initialState = {
   nodes: OrderedMap(),
@@ -8,6 +14,25 @@ const initialState = {
 };
 
 const nodes = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NODE: {
+      const newNode = new NodeContent({
+        id: action.id,
+        text: action.text,
+      });
 
+      return state.set(newNode.id, newNode);
+    }
+    default:
+      return state;
+  }
 };
 
+const nodesInfos = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_NODE:
+      return state.set(action.id, new NodeInfo());
+    default:
+      return state;
+  }
+};
