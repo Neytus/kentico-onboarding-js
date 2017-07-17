f5bb8a3217274040ccdb83dfb33bd15713aee01f
Neytus <jbielik44@gmail.com>
2017-07-17 17:47:37 +0200

    KA-152 Remove commented code

--- onboarding-task/src/components/List.jsx
+++ onboarding-task/src/components/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 const memoize = require('memoizee');
 
 import { AddNodeContainer } from '../containers/AddNode';
@@ -6,66 +7,6 @@ import { createNodeViewModel } from '../models/NodeViewModel';
 import { Node } from './Node';
 
 const createMemoizedViewModel = memoize(createNodeViewModel);
-
-// class List extends PureComponent {
-//   static displayName = 'List';
-//
-//   componentDidMount() {
-//     const { store } = this.context;
-//     this.unsubscribe = store.subscribe(() => this.forceUpdate());
-//   }
-//
-//   componentWillUnmount() {
-//     this.unsubscribe();
-//   }
-//
-//   _deleteNode = id => {
-//     const { store } = this.context;
-//     store.dispatch(actions.deleteNode(id));
-//   };
-//
-//   _onToggle = id => {
-//     const { store } = this.context;
-//     store.dispatch(actions.toggleNode(id));
-//   };
-//
-//   _onSave = (id, text) => {
-//     const { store } = this.context;
-//     store.dispatch(actions.saveNode(id, text));
-//   };
-//
-//   _createNodes = store =>
-//     store.getState().nodesList.nodes
-//       .keySeq()
-//       .map((id, index) => (
-//         <li className="list-group-item" key={id}>
-//           <Node
-//             nodeModel={createMemoizedViewModel(store.getState().nodesList.nodes.get(id), store.getState().nodesList.nodesInfos.get(id), index)}
-//             onSave={this._onSave}
-//             onToggle={this._onToggle}
-//             onDelete={this._deleteNode}
-//           />
-//         </li>)
-//       );
-//
-//   render() {
-//     const { store } = this.context;
-//     const nodes = this._createNodes(store);
-//
-//     return (
-//       <div className="row">
-//         <div className="col-sm-12 col-md-offset-2 col-md-8 ">
-//           <ul className="list-group">
-//             {nodes}
-//             <li className="list-group-item">
-//               <AddNode onAdd={this._addNode} />
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 
 const List = state => {
   const nodes = state.nodesList.nodes
@@ -93,7 +34,7 @@ const List = state => {
 };
 
 List.propTypes = {
-  nodesList: React.PropTypes.object.isRequired,
+  nodesList: PropTypes.object.isRequired,
 };
 
 export { List };
