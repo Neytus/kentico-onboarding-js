28296d7bfbac749685b449f415a1cff42c8256c0
Neytus <jbielik44@gmail.com>
2017-07-17 17:56:45 +0200

    KA-152 Use logger

--- onboarding-task/src/index.js
+++ onboarding-task/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
 import { App } from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import { rootReducer } from './reducers/rootReducer';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
