81ce015bf48dd7cdecf4659cb8e20a0de9f10dff
Neytus <jbielik44@gmail.com>
2017-07-25 12:06:28 +0200

    KA-153 Type List container

--- onboarding-task/src/containers/List.ts
+++ onboarding-task/src/containers/List.ts
@@ -1,9 +1,10 @@
 import { connect } from 'react-redux';
 
 import { List as ListComponent } from '../components/List';
+import { AppState } from '../AppState';
 
-const mapStateToProps = ({ nodesList: { nodesIds } }) => ({
-  nodesIds,
+const mapStateToProps = (state: AppState) => ({
+  nodesIds: state.nodesList.nodesIds,
 });
 
 export const List = connect(mapStateToProps)(ListComponent);
