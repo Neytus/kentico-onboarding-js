bb99a893937bdc51ed654325dd5607f5a8c22b3d
Neytus <jbielik44@gmail.com>
2017-07-25 09:34:14 +0200

    KA-153 Type util functions

--- onboarding-task/src/utils/generateId.ts
+++ onboarding-task/src/utils/generateId.ts
@@ -1,17 +1,17 @@
-const generateId = () => {
-  return generateIdPart() + generateIdPart() + '-'
-    + generateIdPart() + '-'
-    + generateIdPart() + '-'
-    + generateIdPart() + '-'
-    + generateIdPart() + generateIdPart() + generateIdPart();
-};
-
-const generateIdPart = () => {
+const generateIdPart = (): string => {
   let sGuid = '';
   for (let i = 0; i < 4; i++) {
     sGuid += Math.floor(Math.random() * 0xF).toString(0xF);
   }
   return sGuid;
+};
+
+const generateId = (): string => {
+  return generateIdPart() + generateIdPart() + '-'
+    + generateIdPart() + '-'
+    + generateIdPart() + '-'
+    + generateIdPart() + '-'
+    + generateIdPart() + generateIdPart() + generateIdPart();
 };
 
 export { generateId };
--- onboarding-task/src/utils/validation.ts
+++ onboarding-task/src/utils/validation.ts
@@ -1,1 +1,1 @@
-export const isNullOrWhitespace = text => !text || !text.trim().length;
+export const isNullOrWhitespace = (text: string): boolean => !text || !text.trim().length;
