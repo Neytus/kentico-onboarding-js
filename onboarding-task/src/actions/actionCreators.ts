f9c33ab528ef203114416b47a575b52b88a25823
Neytus <jbielik44@gmail.com>
2017-09-01 14:13:46 +0200

    KA-171 Fetch all items

--- onboarding-task/src/actions/actionCreators.ts
+++ onboarding-task/src/actions/actionCreators.ts
@@ -2,10 +2,14 @@ import {
   TOGGLE_NODE,
   SAVE_NODE,
   DELETE_NODE,
+  FETCH_NODES_REQUEST,
+  FETCH_NODES_SUCCESS,
+  FETCH_NODES_FAILURE
 } from './actionTypes';
 import { addNodeFactory } from './addNodeFactory';
 import { generateId } from '../utils/generateId';
 import { IAction } from './IAction';
+import { NodeContent } from '../models/NodeContent';
 
 export const addNode = addNodeFactory(generateId);
 
@@ -31,12 +35,32 @@ export const saveNode = (id: IdType, text: string): IAction => ({
   },
 });
 
+export const fetchNodesRequest = (): IAction => ({
+  type: FETCH_NODES_REQUEST,
+  payload: {}
+});
+
+export const fetchNodesSuccess = (): IAction => ({
+  type: FETCH_NODES_SUCCESS,
+  payload: {}
+});
+
+export const fetchNodesFailure = (): IAction => ({
+  type: FETCH_NODES_FAILURE,
+  payload: {}
+});
+
+const parseNodes = (nodes: Array<object>, dispatch: Dispatch): void => {
+  for (const node of nodes) {
+    let text = (new NodeContent(node)).text;
+    dispatch(addNode(text));
+  }
+};
+
 export const fetchNodes = (): any =>
   (dispatch: any) => {
-    dispatch(addNode('weeeee'));
     return fetch('api/v1/nodes')
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then((json) => parseNodes(json, dispatch))
       .catch((error) => console.log(error));
   };
-
