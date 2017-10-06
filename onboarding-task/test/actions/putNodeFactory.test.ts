c7b97078b0212efeb12c80f409a112e437a7cdd1
Neytus <jbielik44@gmail.com>
2017-10-06 15:27:58 +0200

    KA-171 Unit test putNodeFactory

---
+++ onboarding-task/test/actions/putNodeFactory.test.ts
@@ -0,0 +1,80 @@
+import { putNodeFactory } from '../../src/actions/putNodeFactory';
+
+describe('putNodeFactory', () => {
+
+  const id = 'ef081749-250b-4c35-af07-4ff4364bafa1';
+  const text = 'some random text';
+  const nodeDataToPut = {id, text};
+  const identityFunction = jest.fn((input: any) => input);
+
+  const putRequest = jest.fn(() => ({
+    type: 'PUT_REQUEST'
+  }));
+
+  it('dispatches putNodeRequest action', () => {
+    const myFetch = jest.fn(() => Promise.resolve(nodeDataToPut));
+    const dispatch = identityFunction;
+
+    const putNode = putNodeFactory({
+      putNodeRequest: putRequest,
+      putNodeFetch: myFetch,
+      putNodeSuccess: identityFunction,
+      putNodeFailure: identityFunction,
+    });
+
+    return putNode(nodeDataToPut)(dispatch).then(() => {
+      expect(putRequest.mock.calls.length).toEqual(1);
+      expect(dispatch.mock.calls[0][0]).toEqual({type: 'PUT_REQUEST'});
+    });
+  });
+
+  it('calls putNodeFetch method', () => {
+    const myFetch = jest.fn(() => Promise.resolve(nodeDataToPut));
+    const dispatch = identityFunction;
+
+    const putNode = putNodeFactory({
+      putNodeRequest: putRequest,
+      putNodeFetch: myFetch,
+      putNodeSuccess: identityFunction,
+      putNodeFailure: identityFunction,
+    });
+
+    return putNode(nodeDataToPut)(dispatch).then(() => expect(myFetch.mock.calls.length).toEqual(1));
+  });
+
+  it('dispatches putNodeSuccess action', () => {
+    const myFetch = () => Promise.resolve(new Response(JSON.stringify({ok: true})));
+    const dispatch = identityFunction;
+    const putNodeSuccess = jest.fn(() => 'SUCCESSFULL_UPDATE');
+
+    const putNode = putNodeFactory({
+      putNodeRequest: putRequest,
+      putNodeFetch: myFetch,
+      putNodeSuccess,
+      putNodeFailure: identityFunction,
+    });
+
+    return putNode(nodeDataToPut)(dispatch).then(() => {
+      expect(putNodeSuccess.mock.calls.length).toEqual(1);
+      const dispatchCallArguments = dispatch.mock.calls[1][0];
+
+      expect(dispatchCallArguments).toEqual(nodeDataToPut);
+    });
+  });
+
+  it('dispatches putNodeFailure when necessary', () => {
+    const myFetch = jest.fn(() => Promise.reject(new Response(JSON.stringify({ok: false}))));
+    const dispatch = identityFunction;
+    const putNodeFailure = jest.fn(() => 'deleting failed');
+
+    const putNode = putNodeFactory({
+      putNodeRequest: putRequest,
+      putNodeFetch: myFetch,
+      putNodeSuccess: identityFunction,
+      putNodeFailure: putNodeFailure,
+    });
+
+    return putNode(nodeDataToPut)(dispatch).then(() =>
+      expect(putNodeFailure.mock.calls.length).toEqual(1));
+  });
+});
