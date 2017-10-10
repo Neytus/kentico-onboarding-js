07ed8ee974b439332ac40f54f1968eb5cbe90fb7
Neytus <jbielik44@gmail.com>
2017-10-10 08:41:45 +0200

    KA-171 Unit test all thunk action failures

--- onboarding-task/test/reducers/listReducers/errorReducer.test.ts
+++ onboarding-task/test/reducers/listReducers/errorReducer.test.ts
@@ -2,19 +2,25 @@ import { OrderedMap } from 'immutable';
 
 import * as actions from '../../../src/actions/actionCreators';
 import { errorReducer } from '../../../src/reducers/nodesListReducers/errorReducer';
-import { errorFactory } from '../../../src/actions/addErrorFactory';
 import {
-  FETCH_NODES_FAILURE,
   POST_NODE_FAILURE,
+  FETCH_NODES_FAILURE,
+  PUT_NODE_FAILURE,
+  DELETE_NODE_FAILURE
 } from '../../../src/actions/actionTypes';
 
 describe('errorReducer', () => {
   const emptyState = OrderedMap<Guid, string>();
   const id = '80149842-a624-b66b-5d3c-37c24523ba46';
   const anotherId = '05012399-087d-4944-a742-7cf698e01b85';
-  const generateNewId = jest.fn(() => id);
   const text = 'error text';
   const nonEmptyState = emptyState.set(id, text);
+  const failureActionTypes = [
+    POST_NODE_FAILURE,
+    FETCH_NODES_FAILURE,
+    PUT_NODE_FAILURE,
+    DELETE_NODE_FAILURE
+  ];
 
   it('returns initial state', () => {
     const initialState = undefined;
@@ -43,25 +49,21 @@ describe('errorReducer', () => {
     });
   });
 
-  describe('POST_NODE_FAILURE', () => {
-    it('handles adding a new error call', () => {
-      const postNodeFailure = errorFactory(generateNewId, POST_NODE_FAILURE);
-      const action = postNodeFailure(text);
-
-      const actualState = errorReducer(emptyState, action);
-
-      expect(actualState).toEqual(nonEmptyState);
+  const testFailureAction = (type: string) => {
+    const action = ({
+      type,
+      payload: {
+        id,
+        text
+      }
     });
-  });
 
-  describe('FETCH_NODES_FAILURE', () => {
-    it('handles adding a new error call', () => {
-      const fetchNodesFailure = errorFactory(generateNewId, FETCH_NODES_FAILURE);
-      const action = fetchNodesFailure(text);
+    const actualState = errorReducer(emptyState, action);
 
-      const actualState = errorReducer(emptyState, action);
+    expect(actualState).toEqual(nonEmptyState);
+  };
 
-      expect(actualState).toEqual(nonEmptyState);
-    });
+  it('handles adding a new error call after a failure', () => {
+    failureActionTypes.forEach(testFailureAction);
   });
 });
